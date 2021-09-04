95a89316907606cf0a054537f18d3b3f7a12e5d4
Daniel Rivera <85755386+Driver-glitch@users.noreply.github.com>
2021-09-04 13:06:46 -0400

    not finished

---
+++ src/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profile = () => {
+  return <div className="profile">
+  <h1>Profile</h1>
+  <h2>Name: Daniel</h2>
+  <h2>Email: danielrv997@yahoo.com</h2>
+  <h2>Home: 123 Lane</h2>
+  <h2>Picture: "...."</h2>
+</div>
+};
+
+export default Profile;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Profile from "./Profile";
+import TicTacToe from "./TicTacToe";
+
+const App = (props) => {
+  return (
+    <div className="app">
+      <Profile />
+      <div className="dark-mode">
+        <h1>Dark Mode</h1>
+      </div>
+
+      <div className="tic-tac-toe">
+        <TicTacToe />
+      </div>
+      <div className="todo">
+        <h1>Todo</h1>
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
